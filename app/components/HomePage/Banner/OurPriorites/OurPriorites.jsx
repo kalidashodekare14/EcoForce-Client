df753c103d3ef7fccc7084e40cfabdb4bf1876e4
Kalidash Odekare <shaktiadhikari49@gmail.com>
2025-03-30 14:03:21 +0600

    Our Priorites Section Update

--- app/components/HomePage/Banner/OurPriorites/OurPriorites.jsx
+++ app/components/HomePage/Banner/OurPriorites/OurPriorites.jsx
@@ -1,51 +1,38 @@
 import React from 'react'
 import './OurPriorites.css'
 import img1 from '../../../../images/our_prioties/img1.png'
-import img2 from '../../../../images/our_prioties/img2.jpg'
-import img3 from '../../../../images/our_prioties/img3.jpg'
-import img4 from '../../../../images/our_prioties/img4.jpg'
-import img5 from '../../../../images/our_prioties/img5.jpg'
+import img2 from '../../../../images/our_prioties/img2.png'
+import img3 from '../../../../images/our_prioties/img3.png'
+import img4 from '../../../../images/our_prioties/img4.png'
 
 const OurPriorites = () => {
   return (
     <div className='my-10 lg:mx-32'>
       <h1 className='text-3xl border-b-2 border-[#0077b6] w-48 m-auto'>Our Priorites</h1>
-      <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 my-10'>
-        <div className='relative lg:w-72 mx-2  h-52 flex flex-col justify-center items-center'>
-          <img className='w-full h-full' src={img1} alt="" />
-          <div className='absolute bottom-0 bg-[#000000b4] p-3'>
-            <h1 className='text-white'>Tree Plantation & Afforestation</h1>
-          </div>
+      <div className='grid grid-cols-1 lg:grid-cols-4 gap-5 my-10'>
+        <div className='flex flex-col justify-center items-center gap-2'>
+          <img className='w-20 h-20' src={img1} alt="" />
+          <h1 className='text-2xl'>Tree Plantation</h1>
+          <p className='text-center'>Promoting afforestation to improve air quality and restore ecosystems.</p>
+          <button className='btn bg-[#0077b6] text-white'>Read More</button>
         </div>
-        <div className='relative lg:w-72 mx-2 h-52 flex flex-col justify-center items-center'>
-          <img className='w-full h-full' src={img2} alt="" />
-          <div className='absolute bottom-0 bg-[#000000b4] p-3'>
-            <h1 className='text-white'>Waste Management & Recycling</h1>
-          </div>
+        <div className='flex flex-col justify-center items-center gap-2'>
+          <img className='w-20 h-20' src={img2} alt="" />
+          <h1 className='text-2xl'>Waste Management</h1>
+          <p className='text-center'>Encouraging recycling and proper waste disposal to reduce pollution.</p>
+          <button className='btn bg-[#0077b6] text-white'>Read More</button>
         </div>
-        <div className='relative lg:w-72 mx-2 h-52 flex flex-col justify-center items-center'>
-          <img className='w-full h-full' src={img3} alt="" />
-          <div className='absolute bottom-0 bg-[#000000b4] p-3 '>
-            <h1 className='text-white'>Beach & Public Area Cleaning</h1>
-          </div>
+        <div className='flex flex-col justify-center items-center gap-2'>
+          <img className='w-20 h-20' src={img3} alt="" />
+          <h1 className='text-2xl'>Climate Action</h1>
+          <p className='text-center'>Taking steps to combat climate change and promote sustainability.</p>
+          <button className='btn bg-[#0077b6] text-white'>Read More</button>
         </div>
-        <div className='relative lg:w-72 mx-2 h-52 flex flex-col justify-center items-center'>
-          <img className='w-full h-full' src={img3} alt="" />
-          <div className='absolute bottom-0 bg-[#000000b4] p-3 '>
-            <h1 className='text-white'>Beach & Public Area Cleaning</h1>
-          </div>
-        </div>
-        <div className='relative lg:w-72 mx-2 h-52 flex flex-col justify-center items-center'>
-          <img className='w-full h-full' src={img4} alt="" />
-          <div className='absolute bottom-0 bg-[#000000b4] p-3 '>
-            <h1 className='text-white'>Clean Water & Air Initiative</h1>
-          </div>
-        </div>
-        <div className='relative lg:w-72 mx-2 h-52 flex flex-col justify-center items-center'>
-          <img className='w-full h-full' src={img5} alt="" />
-          <div className='absolute bottom-0 bg-[#000000b4] p-3 '>
-            <h1 className='text-white'>Community Engagement for a Greener Future</h1>
-          </div>
+        <div className='flex flex-col justify-center items-center gap-2'>
+          <img className='w-20 h-20' src={img4} alt="" />
+          <h1 className='text-2xl'>Sustainable Living</h1>
+          <p className='text-center'>Advocating eco-friendly habits for a healthier planet.</p>
+          <button className='btn bg-[#0077b6] text-white'>Read More</button>
         </div>
       </div>
     </div>
--- app/components/HomePage/OurMission/OurMission.jsx
+++ app/components/HomePage/OurMission/OurMission.jsx
@@ -4,12 +4,12 @@ import img2 from '../../../images/about/img2.jpg'
 
 const OurMission = () => {
     return (
-        <div className='mx-32 my-10 flex justify-between items-center gap-3'>
-            <div className='w-[60%]'>
+        <div className='lg:mx-32 mx-3 my-10 flex flex-col lg:flex-row justify-between items-center gap-3'>
+            <div className='lg:w-[60%]'>
                 <img className='w-full rounded-2xl' src={img1} alt="" />
             </div>
-            <div className='w-[60%] space-y-3'>
-                <h1 className='text-3xl'>Our Mission & Vision</h1>
+            <div className='lg:w-[60%] space-y-3'>
+                <h1 className='text-3xl uppercase'>Our Mission & Vision</h1>
                 <h3>Our goal is to create a cleaner, greener, and more sustainable world where nature and humanity coexist in harmony. We are committed to environmental conservation through large-scale tree planting, efficient waste management, and impactful awareness programs to restore nature’s balance and reduce environmental degradation. Our mission is to take effective actions against climate change by promoting eco-friendly practices, advocating for sustainable policies, and encouraging community participation. Through education, innovation, and collective efforts, we aim to build a healthier planet for present and future generations.</h3>
                 <button className='btn bg-[#0077b6] text-white'>Learn More</button>
             </div>
