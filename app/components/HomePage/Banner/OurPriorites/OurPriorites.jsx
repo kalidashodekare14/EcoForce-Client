f4b041c2debb5b6af84a6e9f63cdc24aaccab174
Kalidash Odekare <shaktiadhikari49@gmail.com>
2025-03-30 11:33:59 +0600

    Our mission section implement

--- app/components/HomePage/Banner/OurPriorites/OurPriorites.jsx
+++ app/components/HomePage/Banner/OurPriorites/OurPriorites.jsx
@@ -8,41 +8,41 @@ import img5 from '../../../../images/our_prioties/img5.jpg'
 
 const OurPriorites = () => {
   return (
-    <div className='my-10 mx-32'>
+    <div className='my-10 lg:mx-32'>
       <h1 className='text-3xl border-b-2 border-[#0077b6] w-48 m-auto'>Our Priorites</h1>
-      <div className='grid grid-cols-3 gap-5 my-10'>
-        <div className='relative w-72 flex flex-col justify-center items-center'>
-          <img className='w-full h-52' src={img1} alt="" />
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 my-10'>
+        <div className='relative lg:w-72 mx-2  h-52 flex flex-col justify-center items-center'>
+          <img className='w-full h-full' src={img1} alt="" />
           <div className='absolute bottom-0 bg-[#000000b4] p-3'>
             <h1 className='text-white'>Tree Plantation & Afforestation</h1>
           </div>
         </div>
-        <div className='relative w-72 flex flex-col justify-center items-center'>
-          <img className='w-full h-52' src={img2} alt="" />
+        <div className='relative lg:w-72 mx-2 h-52 flex flex-col justify-center items-center'>
+          <img className='w-full h-full' src={img2} alt="" />
           <div className='absolute bottom-0 bg-[#000000b4] p-3'>
             <h1 className='text-white'>Waste Management & Recycling</h1>
           </div>
         </div>
-        <div className='relative w-72 flex flex-col justify-center items-center'>
-          <img className='w-full h-52' src={img3} alt="" />
+        <div className='relative lg:w-72 mx-2 h-52 flex flex-col justify-center items-center'>
+          <img className='w-full h-full' src={img3} alt="" />
           <div className='absolute bottom-0 bg-[#000000b4] p-3 '>
             <h1 className='text-white'>Beach & Public Area Cleaning</h1>
           </div>
         </div>
-        <div className='relative w-72 flex flex-col justify-center items-center'>
-          <img className='w-full h-52' src={img3} alt="" />
+        <div className='relative lg:w-72 mx-2 h-52 flex flex-col justify-center items-center'>
+          <img className='w-full h-full' src={img3} alt="" />
           <div className='absolute bottom-0 bg-[#000000b4] p-3 '>
             <h1 className='text-white'>Beach & Public Area Cleaning</h1>
           </div>
         </div>
-        <div className='relative w-72 flex flex-col justify-center items-center'>
-          <img className='w-full h-52' src={img4} alt="" />
+        <div className='relative lg:w-72 mx-2 h-52 flex flex-col justify-center items-center'>
+          <img className='w-full h-full' src={img4} alt="" />
           <div className='absolute bottom-0 bg-[#000000b4] p-3 '>
             <h1 className='text-white'>Clean Water & Air Initiative</h1>
           </div>
         </div>
-        <div className='relative w-72 flex flex-col justify-center items-center'>
-          <img className='w-full h-52' src={img5} alt="" />
+        <div className='relative lg:w-72 mx-2 h-52 flex flex-col justify-center items-center'>
+          <img className='w-full h-full' src={img5} alt="" />
           <div className='absolute bottom-0 bg-[#000000b4] p-3 '>
             <h1 className='text-white'>Community Engagement for a Greener Future</h1>
           </div>
--- app/pages/home.jsx
+++ app/pages/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../components/HomePage/Banner/Banner'
 import OurPriorites from '../components/HomePage/Banner/OurPriorites/OurPriorites'
+import OurMission from '../components/HomePage/OurMission/OurMission'
 
 const homePage = () => {
   return (
     <div className=''>
       <Banner />
+      <OurMission />
       <OurPriorites />
     </div>
   )
